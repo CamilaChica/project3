f9686b4a7fb14c060885c5b74b17e643e2be2555
CamilaChica <andrekchir@gmail.com>
2019-11-05 21:50:09 -0500

    declared action types

---
+++ src/constants/actionTypes.js
@@ -0,0 +1,11 @@
+const actionTypes = {
+    ADD_HOUSE: "ADD_HOUSE",
+    SELL_HOUSE: "SELL_HOUSE",
+    OPEN_OPTIONS: "OPEN_OPTIONS",
+    CLOSE_OPTIONS: "CLOSE_OPTIONS",
+    ADD_AGENT: "ADD_AGENT",
+    ADD_SELLER: "ADD_SELLER"
+
+}
+
+export default actionTypes;
