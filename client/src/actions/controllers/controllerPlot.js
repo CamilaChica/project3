683b18b3bd94262803a9244c1b9e23a9c3ae27c9
CamilaChica <andrekchir@gmail.com>
2019-11-11 16:30:35 -0500

    controller plot connected to statistics and functioning

--- client/src/actions/controllers/controllerPlot.js
+++ client/src/actions/controllers/controllerPlot.js
@@ -2,32 +2,26 @@ import React,{Component} from 'react';
 import Plot from '../../components/properties/plot'
 
 class PlotCtrl extends Component {
-        constructor(props){
-                super(props);
-                  this.plotHandler = this.plotHandler.bind(this);
-                    this.state = {
-                        currentMoney: 0,
-                        plotAmount : 0,
-                        plotQuantity: 0,
-                        propertyTotal: 0
-          
-                    }
-                   }
+  constructor(props){
+          super(props);
+            this.plotHandler = this.plotHandler.bind(this);
+             }
 
-        plotHandler() {
-                this.setState((preState) => {
-    
-                    if (preState.currentMoney >= preState.plotAmount){
-                        return {
-                            currentMoney: preState.currentMoney - preState.plotAmount,
-                            plotAmount : preState.plotAmount * 1.5,
-                            plotQuantity: preState.plotQuantity + 1,
-                            propertyTotal: preState.propertyTotal +1
-                            };
-                    }              
-                   });
-              }
-              
+  plotHandler() {
+              if (this.props.currentMoney >= this.props.plotWorth){
+                  this.props.returnCurrentMoney(this.props.currentMoney - this.props.plotWorth);
+
+                  this.props.updateInfo(
+                      "plotAmount",
+                      "plotQuantity",
+                      this.props.plotWorth * 1.5,
+                      this.props.plotQuantity + 1,
+                      this.props.purchaseTotal + 1,
+                      this.props.currentPerSecond + 3
+                  );
+              }                     
+             };
+        
         render()
               {
                 return <Plot action={this.plotHandler} moneyDisplay={this.state.plotAmount} quantity={this.state.plotQuantity}></Plot>
