d6c997c758ecdbc29a2a4c64838ea67e4154293d
CamilaChica <andrekchir@gmail.com>
2019-11-10 22:46:58 -0500

    plot working

--- client/src/actions/controllers/setChanges.js
+++ client/src/actions/controllers/setChanges.js
@@ -1,1 +1,20 @@
+cardClicked=(card)=>{
 
+    if(this.state.clickedCards.includes(card.id))
+    {
+        this.resetGame();
+    }
+    else if((this.state.clickedCards.length+1)===this.state.cards.length)
+    {
+        this.resetGame(true);
+    }
+
+    else
+    {
+        this.setState({
+            cards:randomizeArray(this.state.cards),
+            clickedCards:[...this.state.clickedCards,card.id],
+            score:this.state.score+1,
+            highestScore:Math.max(this.state.score+1,this.state.highestScore)})
+    }
+}
