2cf424a403d4c3ca73048efdbdb69bace43ed2c0
CamilaChica <andrekchir@gmail.com>
2019-11-11 16:36:47 -0500

    plot modified to work with controller

--- client/src/components/properties/plot.js
+++ client/src/components/properties/plot.js
@@ -2,48 +2,24 @@ import React,{Component} from 'react';
 import Player from '../playerStats';
 
 
+
 class Plot extends Component {
   
-  constructor(props){
-    super(props);
-      this.addOne = this.addOne.bind(this);
-        this.state = {
-          count : 0 ,
-          money: 10,
-          currentMoney: ''
-        }
-     }
-
-  
-    handleMoneyInputChange = ({target}) => {
-      const { value: currentMoney } = target;
-      this.setState({ currentMoney })
-    }
-
-     addOne() {                             
-      this.setState((preState) => {
-        return {
-          count : preState.count + 1,
-          money : preState.money * 1.5
-          };
-       });
-     }
-
-    render() {
-      return  <a onClick={this.props.action}>
-      <div class="item_house_cont">
-      
-      <img src="./images/plot.png" alt=""/>
-      
-      <div class="date_cont">
-          <h3>Plot</h3>
-          <img src="./images/dollar.png" alt=""/>
-          <p>{this.props.moneyDisplay}</p>
-      </div>
-      <h2>{this.props.quantity}</h2>
-      </div>
-      </a>
-    }
+  render() {
+    return  <a onClick={this.props.action}>
+    <div class="item_house_cont">
+    
+    <img src="./images/plot.png" alt=""/>
+    
+    <div class="date_cont">
+        <h3>Plot</h3>
+        <img src="./images/dollar.png" alt=""/><p>{this.props.moneyDisplay}</p>
+    </div>
+    <h2>{this.props.plotQuantity}</h2>
+    
+</div>
+</a>
   }
+}
 
-  export default Plot;
+export default Plot;
