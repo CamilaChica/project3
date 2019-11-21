5e411b8404500c89b25875be043c398c2b5af09d
CamilaChica <andrekchir@gmail.com>
2019-11-20 23:58:46 -0500

    some style for login page and mobile responsible :)

--- client/src/components/HomePage/index.js
+++ client/src/components/HomePage/index.js
@@ -20,25 +20,25 @@ authenticate=()=>{
 handleInputChange=(e)=>this.setState({[e.target.name]:e.target.value});
 render ()
 {
-    return <div>
-    <h1> Welcome to Real Estate Game!</h1>
+    return <div className="bg">
+    <h1 className="HP1 bg"> Welcome to Real Estate Game!</h1>
     {!this.props.authenticated&&<div className="row">
-        <div className="col-6 offset-3">
+        <div className="col-6 offset-3 bg">
             <div className="input-group">
                 <input name="email" value={this.state.email} onChange={this.handleInputChange} type="text" name="email" placeholder="Enter your email" className="form-control"/>
             </div>
-            <div className="col-6 offset-3">
+            <div className="col-6 offset-3 inputs">
             <div className="input-group">
                 <input name="password" value={this.state.password} onChange={this.handleInputChange} type="password" name="password" placeholder="Enter your password" className="form-control"/>
             </div>
-            <button onClick={this.authenticate} className="btn btn-primary">Log In</button>
+            <button onClick={this.authenticate} className="btn btn-primary inputs">Log In</button>
         </div>
         </div>
     </div>}
     {this.props.authenticated&&<div className="row">
-        <div className="col-6 offset-3">
-            <h1>Let's play the game!!!</h1>
-            <Link to="/MainCtrl" className="btn btn-primary">Start Game</Link>
+        <div className="col-6 offset-3 bg">
+            <h1 className="HP1">Let's play the game!!!</h1>
+            <Link to="/MainCtrl" className="btn btn-primary ">Start Game</Link>
         </div>
     </div>}
 </div>
