e6c609441502edf52620a1ce2bc2e5cddd893f20
CamilaChica <andrekchir@gmail.com>
2019-11-23 11:26:21 -0500

    databases

--- client/src/App.js
+++ client/src/App.js
@@ -1,101 +1,35 @@
 import React,{Component} from 'react';
-
-import PlayerStats from "./components/playerStats"
-import Options from "./components/options"
-import Items from "./components/items"
-import ItemPlus from "./components/item_plus"
-import BuyAndSell from "./components/buyandsell"
-
-//properties
-
-import Plot from "./components/properties/plot"
-import House1 from "./components/properties/house1"
-import House2 from "./components/properties/house2"
-import Condo from "./components/properties/condo"
-import Building from "./components/properties/building"
-import Mansion from "./components/properties/mansion"
-
-
 import './App.css';
+
+import MainCtrl from "./actions/controllers/mainController"
+import HomePage from './components/HomePage/index';
+
+import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
+
+
+
 class App extends Component{
-    
-
-    constructor(props){
+     constructor(props)
+     {
         super(props);
-          this.moneyHandler = this.moneyHandler.bind(this);
-          //this.toggleHover = this.toggleHover.bind(this);
-            this.state = {
-                currentMoney : 0 ,
-                playerName: "",
-                playerLevel: "",
-                propertyTotal: 0,
-                propertyPuchased: 0,
-                propertySold: 0,
-                clickAmount: 1,
-                moneyPerSecond: 0,
-                plotAmount: 20,
-                house1Amount: 30,
-                house2Amount: 45,
-                condoAmount: 67.5,
-                buidingAmount: 101.25,
-                mansionAmount: 151.875,                                
-            }
-         }
-
-         moneyHandler() {
-            this.setState((preState) => {
-
-                if (valueCoin >= plotAmount){
-                    return {
-                        plotAmount : preState.plotAmount * 1.1
-                        };
-                }
-                
-                
-               });
-          }
-
-         moneyHandler() {
-            this.setState((preState) => {
-                return {
-                  currentMoney : preState.currentMoney + 1
-                  };
-               });
-          }
-
-          buyProperty() {
-            this.setState((preState) => {
-                return {
-                  currentMoney : preState.currentMoney + 1
-                  };
-               });
-          }
-    
-    
+        this.state={
+            authenticated:false,
+            email:""
+        }
+     }
+login=(user)=>{
+this.setState({authenticated:true, email:user.email})
+}
     render()
 {
-    return <div class="row general">
-        <PlayerStats action={this.moneyHandler} moneyDisplay={this.state.currentMoney}></PlayerStats>
-        
-    <div class="col-md-6 p-0">
-        <Options></Options>
-        <Items></Items>
-    </div>
-    <div class="col-md-3 p-0">
-        <div class="row">
-            <ItemPlus></ItemPlus>
-        </div>
-        <BuyAndSell></BuyAndSell>
-        <div class="items_count">
-           <Plot></Plot>
-           <House1></House1>
-           <House2></House2>
-           <Condo></Condo>
-           <Building></Building>
-           <Mansion></Mansion>            
-        </div>
-    </div>
-</div>
+    return <Router>
+    <Switch>
+    {this.state.authenticated&&<Route path="/MainCtrl" component={()=><MainCtrl email={this.state.email} />} />}
+    <Route component ={()=><HomePage onLogIn={this.login} authenticated={this.state.authenticated} />} />
+    </Switch>
+    </Router>;
+
+    
 }
 }
 
--- client/src/components/properties/plot.js
+++ client/src/components/properties/plot.js
@@ -1,47 +1,53 @@
 import React,{Component} from 'react';
-import Player from '../playerStats';
+
 
 
 class Plot extends Component {
   
   constructor(props){
     super(props);
-      this.addOne = this.addOne.bind(this);
+    
         this.state = {
-          count : 0 ,
-          money: 10,
-          currentMoney: ''
+            toolTip:"Plot <br> Produces: "+this.props.plotProductionPercent + "<br> Production per second: " + this.props.plotCoinPerSecond,
+            toggleBlackAndWhite: "grayscale"
         }
-     }
-
   
-    handleMoneyInputChange = ({target}) => {
-      const { value: currentMoney } = target;
-      this.setState({ currentMoney })
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() =>
+    this.setState((prevState, props) => {
+    if(this.props.moneyDisplay> this.props.currentMoney){
+      this.setState(preState =>{ return {toggleBlackAndWhite: "grayscale"}})
+      
+    }else{
+      this.setState(preState =>{ return {toggleBlackAndWhite: "color"}})
+      
     }
 
-     addOne() {                             
-      this.setState((preState) => {
-        return {
-          count : preState.count + 1,
-          money : preState.money * 1.05
-          };
-       });
-     }
+    
+      this.setState(preState =>{
+        return {toolTip:"Plot <br> Produces: "+this.props.plotProductionPercent + " of Total <br> Production per second: " + this.props.plotCoinPerSecond }
+      })
+    }), 1000);
+  }
 
     render() {
-      return  <a onClick={this.addOne}>
+      return  <a onClick={this.props.action}>
       <div class="item_house_cont">
       
-      <img src="./images/plot.png" alt=""/>
+      <img data-tip={this.state.toolTip} class={this.state.toggleBlackAndWhite} src="./images/plot.png" alt=""/>
       
       <div class="date_cont">
+      <audio autoplay className="plot-sound">
+                    <source type="audio/mp3" src="./sounds/house_sound.mp3"></source>
+              </audio>
           <h3>Plot</h3>
-          <img src="./images/dollar.png" alt=""/><p>{this.state.money}</p>
+          <img class={this.state.toggleBlackAndWhite} src="./images/dollar.png" alt=""/>
+          <p>{this.props.moneyDisplay}</p>
       </div>
-      <h2>{this.state.count}</h2>
-      
-  </div>
+          <h2>{this.props.plotQuantity}</h2>
+      </div>
   </a>
     }
   }
